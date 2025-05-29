7b1f2d1613daad00f0ca98e940547f5a8a6ec28d
ASHUTOSH-SHUKLAA <ashutoshmanishukla2006@gmail.com>
2025-05-29 22:20:47 +0530

    Cleaned up Redux store structure

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,18 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
-import appStateSlice from "./features/appStateSlice";
+
+// Slice imports
+import userSlice from "./features/userSlice";
+import themeModeSlice from "./features/themeModeSlice";
 import authModalSlice from "./features/authModalSlice";
 import globalLoadingSlice from "./features/globalLoadingSlice";
-import themeModeSlice from "./features/themeModeSlice";
-import userSlice from "./features/userSlice";
+import appStateSlice from "./features/appStateSlice";
 
+// Slice mapping
+const rootReducer = {
+  user: userSlice,
+  themeMode: themeModeSlice,
+  authModal: authModalSlice,
+  globalLoading: globalLoadingSlice,
+  appState: appStateSlice,
+};
+
+// Store configuration
 const store = configureStore({
-  reducer: {
-    user: userSlice,
-    themeMode: themeModeSlice,
-    authModal: authModalSlice,
-    globalLoading: globalLoadingSlice,
-    appState: appStateSlice
-  }
+  reducer: rootReducer,
 });
 
 export default store;
