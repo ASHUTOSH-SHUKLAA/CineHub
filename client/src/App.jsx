835a509666830f64e089b42f11089f9d882d30e7
ASHUTOSH-SHUKLAA <ashutoshmanishukla2006@gmail.com>
2025-05-29 22:33:22 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from "@mui/material/styles";
 import { useSelector } from "react-redux";
-import themeConfigs from "./configs/theme.configs";
 import { ToastContainer } from "react-toastify";
 import CssBaseline from "@mui/material/CssBaseline";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import themeConfigs from "./configs/theme.configs";
 import MainLayout from "./components/layout/MainLayout";
-import routes from "./routes/routes";
 import PageWrapper from "./components/common/PageWrapper";
+import routes from "./routes/routes";
 
 import "react-toastify/dist/ReactToastify.css";
 import "swiper/css";
